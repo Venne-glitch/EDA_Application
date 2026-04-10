711ff97c916ec29b7791f49172fe0b11b4da3253
Vennela Kache <Kachevennela46@gmail.com>
2026-04-10 11:07:31 +0530

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,9 +1,8 @@
-# Import required packages
 import streamlit as st
 import pandas as pd
 
 # Title and Description
-st.title("🔍 Real-Time Exploratory Data Analysis (EDA) & Statistics Dashboard")
+st.title("🔍 Descriptive Analyzer for Numerical and Categorical Data")
 st.text("This application helps you explore and analyze datasets.")
 st.text("It analyze both categorical and numerical data columns,")
 st.text("It identifies any missing values, viewsstatistical measures and provide an preview of your data structure.")
@@ -14,27 +13,41 @@ st.text("🚀 Upload your dataset and start exploring instantly!")
 file_upload = st.file_uploader("Upload your CSV file (.csv):", type=["csv"])
 if file_upload is not None:
     st.session_state.df = pd.read_csv(file_upload)
-    
+
 # Main logic
 if "df" in st.session_state:
     df = st.session_state.df
-    
-    # Identify columns
+
+    discrete_cols=[]
+    continuous_cols=[]
+    threshold=30
+            
+    # Identify columns (Numerical and Categorical)
     num_cols = df.select_dtypes(include=['int64', 'float64']).columns
     cat_cols = df.select_dtypes(include=['object']).columns
+    # Identify columns (Discrete and Continous)
+    for col in num_cols:
+        if df[col].nunique()>threshold:
+            discrete_cols.append(col)
+        else:
+            continuous_cols.appen(col)
+    st.session_state.discrete_cols=discrete_cols
+    st.session_state.continuous_cols=continuous_cols
+
     st.write("Numerical Columns:", list(num_cols))
     st.write("Categorical Columns:", list(cat_cols))
-    
+    st.write("Discrete Numerical Columns:",discrete_cols)
+    st.write("Continuous Numerical Columns:",continuous_cols)
+
     # Numerical Analysis
     st.subheader("Numerical Analysis")
     select_num_col = st.selectbox("Select Numerical Column", num_cols)
     if st.button("Analyze Numerical Column", key="num_btn"):
         col_data = df[select_num_col]
         if col_data.dropna().empty:
-            st.warning("No non-null values to analyze")
-        # Discrete value condition
-        elif col_data.nunique() <= 25:
-            st.info("ℹ️ This column appears to be discrete")
+            st.warning("⚠️ No non-null values to analyze")
+        elif select_num_col in discrete_cols:
+            st.info("This column appears to be discrete")
             st.write("Value Counts:")
             st.write(col_data.value_counts())
             st.write("Mode:", col_data.mode()[0])
@@ -47,6 +60,7 @@ if "df" in st.session_state:
             st.write("Variance:", col_data.var())
             st.write("Standard Deviation:", col_data.std())
         st.success(f"🎉 Analysis completed for {select_num_col}")
+
     # Categorical Analysis
     st.subheader("Categorical Analysis")
     select_cat_col = st.selectbox("Select Categorical Column", cat_cols)
@@ -57,7 +71,7 @@ if "df" in st.session_state:
         else:
             st.write("Count:", col_data.count())
             st.write("Unique Values:", col_data.nunique())
-            if col_data.nunique() > 25:
+            if select_cat_col in discrete_cols:
                 st.warning("Discrete value column")
             st.write("Value Counts:")
             st.write(col_data.value_counts())
